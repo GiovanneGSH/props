3a6626d4aa1b8728ef7bbc43df8ef4989ae8a0ee
GiovanneGSH <giovannegsh@gmail.com>
2025-05-29 09:09:34 -0300

    primeiro commmit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Corpo from './components/corpo'
+import Olhos from './components/Olhos'
+function App() {
+
+
+  return (
+    <div>
+      <Corpo/>
+      <Olhos/>
+    </div>
+   
+  )
+}
+
+export default App
